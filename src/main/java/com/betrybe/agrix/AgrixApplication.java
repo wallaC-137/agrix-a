41c297a8abcaedd2119eeb7ed1a6bb1175877a2c
wallaC-137 <wallace_031@hotmail.com>
2023-09-13 07:17:44 -0300

    iniciando

--- src/main/java/com/betrybe/agrix/AgrixApplication.java
+++ src/main/java/com/betrybe/agrix/AgrixApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class AgrixApplication {
-
+  // iniciando projeto
   public static void main(String[] args) {
     SpringApplication.run(AgrixApplication.class, args);
   }
